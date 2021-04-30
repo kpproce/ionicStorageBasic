1fa624d1fbf8398c5a4ffdb4d1a25ab88fff3f69
KPR <64140329+kpproce@users.noreply.github.com>
2021-04-30 09:06:19 +0200

    master

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  tempName = 'Leeg';
+  tempAge = 21;
 
+  constructor(private storage: Storage) { }
+
+  async ngOnInit() {
+    await this.storage.create();
+  }
+
+  async setItem() {
+    alert (this.tempName + ' opgeslagen in storage in de var tempName');
+    await this.storage.set('name', this.tempName);
+  }
+
+  async getItem() {
+    this.tempName = await this.storage.get('name');
+  }
+
+  defaultItem() {
+      this.tempName = 'Leeg gemaakt';
+  }
 }
